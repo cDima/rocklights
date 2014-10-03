0fe5a25447dadc81ffd41342986d69e804453f2f
Dmitry Sadakov <sadakov@gmail.com>
2014-10-02 22:52:48 -0400

    Adding circle canvas

--- js/client.js
+++ js/client.js
@@ -1,12 +1,14 @@
 
 var host = location.origin.replace(/^http/, 'ws')
-var ws = new WebSocket(host);
+
 
 moment.defaultFormat = 'h:mm:ss:SSS a';
 
 var timeBefore = new moment();
 document.querySelector('#serverTime').innerHTML = timeBefore.format();
 
+try {
+var ws = new WebSocket(host);
 ws.onmessage = function (event) {
 
   var serverData = JSON.parse(event.data);
@@ -39,3 +41,86 @@ ws.onmessage = function (event) {
   document.querySelector('#color').innerHTML = color;
 
 };
+
+} catch (ex)
+{
+  // localhost would fail 
+}
+
+
+  var canvas = document.getElementById('canvas');
+  var context = canvas.getContext('2d');
+  var centerX = canvas.width / 2;
+  var centerY = canvas.height / 2;
+  var radius = 70;
+
+
+  window.setInterval(timerHit, 20);
+
+
+  function timerHit(){
+    var drawStart = new Date().getTime();
+
+    draw();
+
+    var drawEnd = new Date().getTime();
+    var drawTime = drawEnd - drawStart;
+
+    document.querySelector('#drawlag').innerHTML = drawTime;
+  }
+
+  function draw(){
+    
+    //drawCircle(1, 1);
+    //return;
+
+    radius++;
+    if (radius > (canvas.width)) {
+      radius = 0;
+      context.clearRect(0, 0, canvas.width, canvas.height);
+    }
+    
+    clear();
+    drawCircle(radius, 40, "white");
+
+    drawCircle(Math.max(0, radius - 40), 40, "black");
+    drawGrid();
+    //drawCircle(radius2);
+  }
+
+  function clear(){
+    context.beginPath();
+    context.strokeStyle = "#00ff00";
+    context.strokeRect(0,0, canvas.width, canvas.height);
+  }
+
+  function drawCircle(radius, thick, color){
+    context.beginPath();
+    if (radius < thick) { thick = radius; }
+    context.lineWidth = thick;
+    context.arc(centerX, centerY, radius, 0, 2 * Math.PI);
+    context.strokeStyle = color;
+    context.stroke();
+  }
+
+  function line(x0,y0,x1,y1,width){
+    context.beginPath();
+    context.strokeStyle = 'black';
+    context.lineWidth = width;
+    context.moveTo(x0, y0); 
+    context.lineTo(x1,y1);
+    context.closePath();
+    context.stroke();
+  }
+
+  function drawGrid(){
+    //line(0, 1, canvas.width, 6);
+    for (var y = 0; y <= canvas.width; y += 4) {
+      line(0, y, canvas.width, y, 2);
+    }
+
+    for (var x = 0; x <= canvas.height; x += 4) {
+      line(x, 0, x, canvas.height, 2);
+    }
+
+  }
